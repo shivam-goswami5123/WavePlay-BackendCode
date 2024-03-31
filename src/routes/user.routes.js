a0cc2ae90b086d7c709cc9473266947c6b0423ff
shivam-goswami5123 <shivam.goswami.ug22@nsut.ac.in>
2024-03-31 17:53:33 +0530

    add more user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,7 @@
 import {Router} from "express";
-import {registerUser,loginUser,logoutUser,refreshAccessToken} from "../controllers/user.controller.js";
+import {registerUser,loginUser,logoutUser,refreshAccessToken,changeUserPassword,getCurrentUser
+    ,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,
+    getWatchHistory} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -27,6 +29,21 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT,changeUserPassword);
+router.route("/current-user").get(verifyJWT,getCurrentUser);
+router.route("/update-account-details").patch(verifyJWT,updateAccountDetails); //patch route because needs to update only few fields
+router.route("/update-avatar").patch(
+    verifyJWT, //firstly user should be loggedIn then update avatar
+    upload.single("avatar"), //want to upload single file (not used upload.fields() as above)
+    updateUserAvatar);
+router.route("/update-coverimage").patch(
+    verifyJWT,
+    upload.single("coverImage"),
+    updateUserCoverImage);
+
+//getting information from url : from req.params
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile); 
+router.route("/watch-history").get(verifyJWT,getWatchHistory);
 
 
 
