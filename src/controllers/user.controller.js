dd13ba4da10fd5cf682e2df3a1b4c14d9b819bf8
shivam-goswami5123 <shivam.goswami.ug22@nsut.ac.in>
2024-03-31 17:33:51 +0530

    Add watchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -383,9 +383,155 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
     .json(
         new ApiResponse(200,user,"CoverImage Changed Successfully")
     )
+});
+
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+    const {username} = req.params;
+
+    if(!username?.trim()){
+        throw new ApiError(404,"User Not Found");
+    }
+
+
+    const channel=await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions", //name of model as stored in mongo db
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subcribedTo"
+            }
+        },
+        {
+            //add more fields to each document of user model
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers" //as 'subscribers' is field name so use '$' before it
+                },
+                channelsSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    //read more about it
+                    $cond:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            //make the flag 1 to those fields which u want to project
+            $project:{
+                fullName:1,
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1
+            }
+        }
+    ])
+
+    console.log(channel);
+
+    if(!channel?.length){
+        throw new ApiError(404,"Channel Does Not Exist");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            channel[0],
+            "User Channel Fetched Successfully"
+        )
+    );
+});
+
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    //match the user
+    //write pipelines : subpipelines
+
+    const user=await User.aggregate([
+        {
+            $match:{
+                //req.user?._id : not used this because aggregation commands directly goes to mongodb  without interference of mongoose 
+                //so to get _id of correct format we use this below command
+                //actual output of req.user._id is string of just below format
+                //database : this is stored in document of a model {_id:ObjectId('6753b.....')}
+                //but with mongoose interference behind the scenes it gives only 6753b....
+                _id:new mongoose.Types.ObjectId(req.user?._id) 
+                
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            //TODO:Wrie this pipeline outside and experiment what happens
+                            //made this pipeline because while lookup : videos"owner" from users"_id" we will get many fields from users but 
+                            //we requires only some fields so by this pipeline we project only few required fields to owner
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        //array in output form but for frontend easy access use this below code
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,user[0].watchHistory,"Watch History Fetched Successfully")
+    )
 })
 
 //after getting control from user.routes.js , registerUser function runs and user will be registered
 
 export {registerUser,loginUser,logoutUser,refreshAccessToken,changeUserPassword,getCurrentUser
-,updateAccountDetails,updateUserAvatar,updateUserCoverImage};
+,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,
+getWatchHistory};
